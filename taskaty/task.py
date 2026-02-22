846a4553c1da49a8a61ad4fc2cdd11761b429485
Sura Saber <YOUR_GITHUB_EMAIL@example.com>
2026-02-22 10:46:50 +0300

    Fix typo in description

--- taskaty/task.py
+++ taskaty/task.py
@@ -1,13 +1,13 @@
 
 class Task:
-    def __init__(self ,title ,describtion=None ,start_date=None ,end_date=None ,done=False):
+    def __init__(self ,title ,description=None ,start_date=None ,end_date=None ,done=False):
 
         self.title=title
-        self.describtion=describtion
+        self.description=description
         self.start_date=start_date
         self.end_date=end_date
         self.done=done
 
 
     def __str__(self) :
-        return f'{self.title}, {self.describtion}, {self.start_date}, {self.end_date}, {self.done}'
+        return f'{self.title}, {self.description}, {self.start_date}, {self.end_date}, {self.done}'
